77133bb2950af19819afd99b17026eabdb16fd4c
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-04 00:48:49 +0700

    Deploy to Heroku

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm -rf dist  # Ensure that dist folder isn't exist in the first place
+git branch master heroku
 git worktree add -f dist heroku
 sed '/aloud\.config\.js/d' .gitignore > dist/.gitignore
 cp aloud.config.js dist/aloud.config.js
@@ -8,3 +9,4 @@ git commit -m 'Deploy to Heroku'
 git push -f heroku heroku:master
 cd ..
 git worktree remove dist
+git branch -D heroku
