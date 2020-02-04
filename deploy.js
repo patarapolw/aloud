898017175c5b954bf529d894ac8acbbd4d6ee558
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-04 15:26:47 +0700

    commit pour to GitHub

--- deploy.js
+++ deploy.js
@@ -1,37 +1,6 @@
 /* eslint-disable no-console */
-const { spawn } = require('child_process')
 const fs = require('fs')
-
-/**
- *
- * @param {string} cmd
- * @param {string[]} [args]
- * @param {typeof import('child_process').SpawnOptionsWithoutStdio} [options]
- */
-async function Pour (cmd, args, options) {
-  const p = this.process = spawn(cmd, args, options)
-  console.log('\x1B[2;37m', [cmd, ...(args || [])].map((el) => {
-    return el.includes(' ') ? `"${el.replace(/"/g, '\\"')}"` : el
-  }).join(' '), '\x1B[0m')
-
-  return new Promise((resolve, reject) => {
-    p.stdin.pipe(process.stdin)
-    p.stdout.on('data', d => console.log(d.toString().trimEnd()))
-    p.stderr.on('data', d => console.error(d.toString().trimEnd()))
-    p.on('error', reject)
-    p.on('close', resolve)
-  })
-}
-
-/**
- *
- * @param {string} cmd
- * @param {typeof import('child_process').SpawnOptionsWithoutStdio} [options]
- */
-async function PourSimple (cmd, options) {
-  const [args0, ...args] = cmd.split(' ')
-  return Pour(args0, args, options)
-}
+const { pour } = require('pour-console')
 
 /**
  *
@@ -54,23 +23,24 @@ async function deploy (
   deployMessage = 'Deploy to Heroku'
 ) {
   // Ensure that dist folder isn't exist in the first place
-  await PourSimple('rm -rf dist')
+  await pour('rm -rf dist')
 
   try {
-    await PourSimple(`git branch ${deployBranch} master`)
+    await pour(`git branch ${deployBranch} master`)
   } catch (e) {
     console.error(e)
   }
 
-  await PourSimple(`git worktree add -f ${deployFolder} ${deployBranch}`)
+  await pour(`git worktree add -f ${deployFolder} ${deployBranch}`)
 
   await callback(deployFolder, deployBranch)
 
-  await PourSimple('git add .', {
+  await pour('git add .', {
     cwd: deployFolder
   })
 
-  await Pour('git', [
+  await pour([
+    'git',
     'commit',
     '-m',
     deployMessage
@@ -78,13 +48,13 @@ async function deploy (
     cwd: deployFolder
   })
 
-  await PourSimple(`git push -f heroku ${deployBranch}:master`, {
+  await pour(`git push -f heroku ${deployBranch}:master`, {
     cwd: deployFolder
   })
 
-  await PourSimple(`git worktree remove ${deployFolder}`)
+  await pour(`git worktree remove ${deployFolder}`)
 
-  await PourSimple(`git branch -D ${deployBranch}`)
+  await pour(`git branch -D ${deployBranch}`)
 }
 
 deploy(async (deployFolder) => {
