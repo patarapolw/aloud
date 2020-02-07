a35752c4d9c472f13a0739d7a95b8937ae7ef135
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-07 09:08:38 +0700

    add staging push-secrets

--- push-secrets.js
+++ push-secrets.js
@@ -6,4 +6,6 @@ const { flattenConfig } = require('./server/utils')
 pour(`heroku config:set ${
   Object.entries(flattenConfig()).map(([k, v]) => `${k}=${v}`).join(' ')
 // eslint-disable-next-line no-console
+} ${
+  process.env.REMOTE ? `--remote ${process.env.REMOTE}` : ''
 }`).catch(console.error).then(() => process.exit())
