4c8f066ac2d2249cf4eb41d515422d514073e245
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-07 09:06:41 +0700

    update push-secrets

--- push-secrets.js
+++ push-secrets.js
@@ -6,4 +6,4 @@ const { flattenConfig } = require('./server/utils')
 pour(`heroku config:set ${
   Object.entries(flattenConfig()).map(([k, v]) => `${k}=${v}`).join(' ')
 // eslint-disable-next-line no-console
-}`).catch(console.error)
+}`).catch(console.error).then(() => process.exit())
