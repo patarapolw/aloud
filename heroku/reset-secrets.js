197e5e4fa98dea7c7bf2d7ec4d9d285cdc81d80b
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-08 11:43:15 +0700

    update

--- heroku/reset-secrets.js
+++ heroku/reset-secrets.js
@@ -2,11 +2,13 @@
 require = require('esm')(module)
 const { execSync } = require('child_process')
 
-const { pour } = require('pour-console');
+const { pour } = require('pour-console')
+
+const setRemote = process.env.REMOTE ? `--remote ${process.env.REMOTE}` : '';
 
 (async () => {
-  const envKeys = execSync(`heroku config --remote ${process.env.REMOTE}`).toString().trim().split('\n')
+  const envKeys = execSync(`heroku config ${setRemote}`).toString().trim().split('\n')
     .filter(row => row.includes(':')).map(row => row.split(':')[0])
 
-  await pour(`heroku config:unset ${envKeys.join(' ')} --remote ${process.env.REMOTE}`)
+  await pour(`heroku config:unset ${envKeys.join(' ')} ${setRemote}`)
 })().catch(console.error).then(() => process.exit())
