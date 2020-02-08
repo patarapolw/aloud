363e0df0dec06bc315b0f7f5ef9ed05522eecae9
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-08 12:24:20 +0700

    fix logging out and noreply in logged-out

--- server/api/user.js
+++ server/api/user.js
@@ -2,20 +2,21 @@ import { Router } from 'express'
 import secureMiddleware from '../middleware/secure'
 
 const userRouter = Router()
-userRouter.use(secureMiddleware)
+// userRouter.use(secureMiddleware)
 
-userRouter.get('/', (req, res) => {
+userRouter.get('/', secureMiddleware, (req, res) => {
   res.json(req.user)
 })
 
-userRouter.post('/login', (req, res) => {
+userRouter.post('/login', secureMiddleware, (req, res) => {
   req.session.user = req.session.user || req.body
   res.sendStatus(201)
 })
 
-userRouter.delete('/logout', (req, res) => {
-  req.session.destroy()
-  res.sendStatus(201)
+userRouter.delete('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    err ? next(err) : res.sendStatus(201)
+  })
 })
 
 export default userRouter
--- store/index.js
+++ store/index.js
@@ -1,6 +1,10 @@
 export const actions = {
   async nuxtServerInit ({ commit }, { req: { session: { token, user } } }) {
-    commit('auth/addUser', user)
-    commit('auth/addToken', token)
+    // eslint-disable-next-line no-console
+    console.log(user)
+    if (user) {
+      commit('auth/addUser', user)
+      commit('auth/addToken', token)
+    }
   }
 }
